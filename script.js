0f37e5fda864a7dd39931085a2e5b708814a1277
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-01-10 22:26:43 +0530

    minimal change on script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(function (req, res, next) {
+  console.log("hello from middleware");
+  next();
+});
 
 app.get("/", function (req, res) {
   res.send("Linkedin");
 });
 
-app.ser
+app.get("/profile", function (req, res) {
+  res.send("Harsh Singh Mumbai");
+});
 app.listen(3000);
